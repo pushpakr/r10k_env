02f200cf2b33b7d7fdf5544a933c791f66bd058d
root <root@puppet.india.xoriantcorp.com>
2016-07-18 17:46:43 +0530

    site.pp sites update

---
+++ manifests/site.pp
@@ -0,0 +1,11 @@
+node default {
+  # This is where you can declare classes for all nodes.
+  # Example:
+  #   class { 'my_class': }
+}
+
+node puppet.india.xoriantcorp.com {
+}
+node "mylab.xoriantlinux.net" {
+	include host_website
+}
